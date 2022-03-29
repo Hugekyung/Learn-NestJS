11b440e83be8924a1a557a92cba30da3b4d5a2de
haechan <kiki9510@gmail.com>
2022-03-30 00:19:24 +0900

    Added createBoard api in controller

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
 import { BoardsService } from "./boards.service";
 import { Board } from "./board.model";
 
@@ -6,8 +6,18 @@ import { Board } from "./board.model";
 export class BoardsController {
     constructor(private boardsService: BoardsService) {}
 
-    @Get()
+    @Get("/")
     getAllBoard(): Board[] {
         return this.boardsService.getAllBoards();
+    }
+
+    @Post()
+    createBoard(
+        // 클라이언트에서 보낸 request의 body값은 @Body() body를 통해 얻을 수 있다
+        // 아래와 같이 body 전체가 아닌 특정 값만 얻기 위해 쓸 수도 있다
+        @Body("title") title: string,
+        @Body("description") description: string,
+    ): Board {
+        return this.boardsService.createBoard(title, description);
     }
 }
