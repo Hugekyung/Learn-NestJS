b85b8fba8b1e0eebe0e0e80e729057316812b745
haechan <kiki9510@gmail.com>
2022-03-30 19:33:23 +0900

    Added Validation Error handling

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,24 @@
+import { PipeTransform, BadRequestException } from "@nestjs/common";
+import { BoardStatus } from "../board.model";
+
+// * 커스텀 파이프라인을 만드려면 PipeTransform이라는 인터페이스를 상속받아야 한다
+export class BoardStatusValidationPipe implements PipeTransform {
+    readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+    transform(value: any) {
+        value = value.toUpperCase();
+
+        if (!this.isStatusValid(value)) {
+            throw new BadRequestException(
+                `${value} is not a valid board status`,
+            );
+        }
+
+        return value;
+    }
+
+    private isStatusValid(status: any) {
+        const index = this.StatusOptions.indexOf(status);
+        return index !== -1;
+    }
+}
