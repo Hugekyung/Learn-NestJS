19d4cf96934a0ffb3bf29d0649fb3173823a7546
haechan <kiki9510@gmail.com>
2022-03-31 23:56:49 +0900

    Added class-validator in Auth dto

--- src/auth/dto/auth-credentialsDto.dto.ts
+++ src/auth/dto/auth-credentialsDto.dto.ts
@@ -1,9 +1,24 @@
-import { IsNotEmpty } from "class-validator";
+import {
+    IsNotEmpty,
+    IsString,
+    Matches,
+    MaxLength,
+    MinLength,
+} from "class-validator";
 
 export class AuthCredentialsDto {
     @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
 
     @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+    @Matches(/^[a-zA-Z0-9]*$/, {
+        message: "password must be english and number.",
+    }) // 정규표현식 패턴을 넣어 해당 패턴에 해당하는 값만 넣을 수 있게 한다
     password: string;
 }
