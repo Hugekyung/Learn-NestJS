da7829e4c39c2d5c3773d4f2eedc0da837abd89a
haechan <kiki9510@gmail.com>
2022-04-02 23:35:14 +0900

    Added error handling for unique username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,7 @@
+import {
+    ConflictException,
+    InternalServerErrorException,
+} from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentialsDto.dto";
 import { User } from "./user.entity";
@@ -8,6 +12,14 @@ export class UserRepository extends Repository<User> {
         const { username, password } = authCredentialsDto;
         const user = this.create({ username, password });
 
-        await this.save(user);
+        try {
+            await this.save(user);
+        } catch (error) {
+            if (error.code === "23505") {
+                throw new ConflictException("이미 존재하는 username입니다.");
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 }
