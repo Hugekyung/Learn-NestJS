ea8be05adf55f106e1eabfe3ccf5ccf69216026e
haechan <kiki9510@gmail.com>
2022-04-02 23:28:54 +0900

    Added Unique decorator to username

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+    BaseEntity,
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+    Unique,
+} from "typeorm";
 
 @Entity()
+@Unique(["username"]) // 배열에다 유니크하게 설정할 컬럼을 지정한다
 export class User extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
