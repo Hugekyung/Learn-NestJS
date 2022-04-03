d3357ee49d2400acce114fae0be4119e599a36c3
haechan <kiki9510@gmail.com>
2022-04-03 17:34:42 +0900

    Added UseGuard decorator for authentication

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Post, ValidationPipe } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Get,
+    Post,
+    Req,
+    UseGuards,
+    ValidationPipe,
+} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "./auth.service";
 import { AuthCredentialsDto } from "./dto/auth-credentialsDto.dto";
 
@@ -18,5 +27,11 @@ export class AuthController {
         @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
     ): Promise<{ accessToken: string }> {
         return this.authService.signIn(authCredentialsDto);
+    }
+
+    @Post("/test")
+    @UseGuards(AuthGuard()) // 인증처리 -> AuthGuard는 passport에서 req.user객체에 유저정보를 넣어주는 역할 처리
+    test(@Req() req) {
+        console.log("req >> ", req.user);
     }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { PassportModule } from "@nestjs/passport";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { JwtStrategy } from "./jwt.strategy";
 import { UserRepository } from "./user.repository";
 
 @Module({
@@ -18,6 +19,9 @@ import { UserRepository } from "./user.repository";
         TypeOrmModule.forFeature([UserRepository]),
     ], // UserRepository를 사용하기 위해 선언
     controllers: [AuthController],
-    providers: [AuthService],
+    // JwtStrategy를 Auth 모듈 내에서 사용할 수 있도록 등록
+    providers: [AuthService, JwtStrategy],
+    // 다른 모듈들에서도 사용할 수 있도록 exports에 등록
+    exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
