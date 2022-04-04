b8ffd18f32eb88d61feece6fecec1ebc88ab80a5
haechan <kiki9510@gmail.com>
2022-04-04 18:20:20 +0900

    config to jwt & typeorm.config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { User } from "./user.entity";
 import { UserRepository } from "./user.repository";
+import * as config from "config";
 
 @Injectable() // 다른 ts 파일에서 주입할 수 있음을 지정하는 데코레이터
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
         private userRepository: UserRepository,
     ) {
         super({
-            secretOrKey: "SecretJWT1234",
+            secretOrKey: process.env.JWT_SECRET || config.get("jwt.secret"),
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
         });
     }
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as config from "config";
+
+const dbConfig = config.get("db");
 
 export const typeORMConfig: TypeOrmModuleOptions = {
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "board-app",
+    type: dbConfig.type,
+    host: process.env.RDS_HOSTNAME || dbConfig.host,
+    port: process.env.RDS_PORT || dbConfig.port,
+    username: process.env.RDS_USERNAME || dbConfig.username,
+    password: process.env.RDS_PASSWORD || dbConfig.password,
+    database: process.env.RDS_DB_NAME || dbConfig.database,
     entities: [__dirname + "/../**/*.entity.{js,ts}"],
-    synchronize: true, // 애플리케이션 실행 시 엔티티 안에서 수정된 컬럼 길이, 타입변경 등을 반영항 테이블 생성
+    synchronize: dbConfig.synchronize, // 애플리케이션 실행 시 엔티티 안에서 수정된 컬럼 길이, 타입변경 등을 반영항 테이블 생성
 };
