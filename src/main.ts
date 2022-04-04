d41939e87b5c1f643c447cad8c276527ff85bf81
haechan <kiki9510@gmail.com>
2022-04-04 18:07:19 +0900

    Added config with yaml files

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as config from "config";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    const port = 3000;
+    const port = config.get("server").port;
     await app.listen(port);
     Logger.log(`Application running on port ${port}`);
 }
